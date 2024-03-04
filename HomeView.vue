55b2f21dda2e930bf26461439cfef0cbc706d900
NellitG <123721407+NellitG@users.noreply.github.com>
2024-03-04 16:53:53 +0300

    Add files via upload

---
+++ HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <Hero />
+  <Clients />
+
+  <Faq />
+</template>
+
+<script setup>
+import Hero from "../components/Hero.vue";
+import Clients from "../components/Clients.vue";
+import Faq from "@/components/Faq.vue";
+</script>
